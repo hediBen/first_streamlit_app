27e6fc855f5cf310c28f53c06072ac4cfd68fb70
HediBenNaceur <hedi.bennaceur@esprit.tn>
2023-09-13 15:00:05 +0200

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
-streamlit.title('test')
-streamlit.header('testt222')
+streamlit.title('My parents new healthy menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
